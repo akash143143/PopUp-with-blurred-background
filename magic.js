ce0b8cb544a659ba14b02c96aa58fa22328743bf
Akash Mailar <72142466+akash143143@users.noreply.github.com>
2024-04-23 14:44:58 +0530

    Add files via upload

---
+++ magic.js
@@ -0,0 +1,7 @@
+function toggle(){
+    let blur = document.querySelector('.container');
+    blur.classList.toggle('blur');
+
+    let popUp = document.querySelector('.popUp');
+    popUp.classList.toggle('active');
+}
